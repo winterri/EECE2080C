cc11a388d3ea61fe32edf5d20db1236cd27bdb3b
winterri <55256054+winterri@users.noreply.github.com>
2020-05-14 12:30:02 -0400

    Add files via upload

---
+++ HW5_2_Team218_winterri.m
@@ -0,0 +1,24 @@
+% Project 4
+% File: Proj4_EIAM_19spr_Team_218.m
+% Date: 9 Mar 2019
+% By: Ryan Winterhalter
+%		winterri
+% Section: 017
+% Team: 218
+%
+% ELECTRONIC SIGNATURE
+% Ryan Winterhalter
+%
+% The electronic signature above indicates that the script
+% submitted for evaluation is my individual work, and I 
+% have a general understanding of all aspects of its
+% development and execution.
+%
+% A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+% This program will ask the user to input a certain number of variables
+% that the program will use to display the final cost and demand of the
+% model for bringing an iceberg to Freetown Port in Sierra Leone
+
+clc;
+clear;
+
